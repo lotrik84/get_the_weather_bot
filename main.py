7978a85d482da0078b0566780142b27520e54c1e
Oleksandr Pastukhov <lotrikys@gmail.com>
2024-09-02 12:48:36 +0300

    * Change API calls and parser

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from datetime import date
 from datetime import datetime
 import locale
 import emoji
-import weatherAPI
+import weatherAPI as get_weather_api
 from weather_logs import weather_logs
 from usersCities import get_user_cities, update_user_cities
 
@@ -20,12 +20,12 @@ start = types.BotCommand("start", "Головна")
 m_c_weather = types.BotCommand("weather", "Поточна погода")
 m_1d_weather = types.BotCommand("todayweather", "Погода на сьогодні")
 m_3d_weather = types.BotCommand("3daysweather", "Погода на 3 дні")
-m_7d_weather = types.BotCommand("weekweather", "Погода на тиждень")
+m_7d_weather = types.BotCommand("5daysweather", "Погода на 5 днів")
 bot.set_my_commands(
     commands=[start, m_c_weather, m_1d_weather, m_3d_weather, m_7d_weather],
 )
-get_weather_api = weatherAPI
-weather_hours = ["03", "06", "09", "12", "15", "18", "21"]
+
+weather_hours = ["03", "06", "09", "12", "15", "18", "21", "00"]
 
 
 @bot.message_handler(commands=["start"])
@@ -40,17 +40,17 @@ def greet(message):
 def current_weather(message):
     # bot.delete_message(call.message.chat.id, call.message.id)
     locale.setlocale(locale.LC_TIME, "uk_UA.UTF-8")
-    response = get_weather_api.get_weather(message.text)
+    response = get_weather_api.get_weather(message.text, "weather")
     markup = telebot.types.ReplyKeyboardRemove()
     if response:
         # print(response)
-        temp = round(response["current"]["temp"])
-        humidity = round(response["current"]["humidity"])
-        wind_speed = round(response["current"]["wind_speed"])
-        pressure = round(response["current"]["pressure"] * 0.75006)
-        weather_emoji = get_emoji.getemoji(response["current"]["weather"][0]["id"])
-        description = response["current"]["weather"][0]["description"]
-        time_stamp = datetime.fromtimestamp(response["current"]["dt"]).strftime("%H:%M")
+        temp = round(response["main"]["temp"])
+        humidity = round(response["main"]["humidity"])
+        wind_speed = round(response["wind"]["speed"])
+        pressure = round(response["main"]["pressure"] * 0.75006)
+        weather_emoji = get_emoji.getemoji(response["weather"][0]["id"])
+        description = response["weather"][0]["description"]
+        time_stamp = datetime.fromtimestamp(response["dt"]).strftime("%H:%M")
         bot.send_message(
             message.chat.id,
             f"Поточна погода у місті {message.text.title()}:\n\n{time_stamp} {weather_emoji} "
@@ -67,7 +67,7 @@ def current_weather(message):
 
 def today_weather(message):
     locale.setlocale(locale.LC_TIME, "uk_UA.UTF-8")
-    response = get_weather_api.get_weather(message.text)
+    response = get_weather_api.get_weather(message.text, "forecast")
     markup = telebot.types.ReplyKeyboardRemove()
     if response:
         temp = {}
@@ -80,7 +80,7 @@ def today_weather(message):
         msg = f'Погода на сьогодні {date.today().strftime("%d %B, %A").title()} у місті {message.text.title()}:\n'
         day_today = date.today().strftime("%d")
 
-        for time in response["hourly"]:
+        for time in response["list"]:
             hour = datetime.fromtimestamp(time["dt"]).strftime("%H")
             day = datetime.fromtimestamp(time["dt"]).strftime("%d")
             if (
@@ -89,10 +89,10 @@ def today_weather(message):
                 or (int(day) == int(day_today) + 1 and hour == "00")
             ):
                 hours.append(hour)
-                temp[hour] = round(time["temp"])
-                humidity[hour] = round(time["humidity"])
-                wind_speed[hour] = round(time["wind_speed"])
-                pressure[hour] = round(time["pressure"] * 0.75006)
+                temp[hour] = round(time["main"]["temp"])
+                humidity[hour] = round(time["main"]["humidity"])
+                wind_speed[hour] = round(time["wind"]["speed"])
+                pressure[hour] = round(time["main"]["pressure"] * 0.75006)
                 weather_emoji[hour] = get_emoji.getemoji(time["weather"][0]["id"])
                 description[hour] = time["weather"][0]["description"]
         for hour in hours:
@@ -112,7 +112,7 @@ def today_weather(message):
 
 def couple_days_weather(message, c_days):
     locale.setlocale(locale.LC_TIME, "uk_UA.UTF-8")
-    response = get_weather_api.get_weather(message.text)
+    response = get_weather_api.get_weather(message.text, "forecast")
     markup = telebot.types.ReplyKeyboardRemove()
     if response:
         temp_day = {}
@@ -123,21 +123,31 @@ def couple_days_weather(message, c_days):
         weather_emoji = {}
         description = {}
         days = []
+        day_today = date.today().strftime("%d")
         if c_days == 3:
             msg = f"Погода на найближчі 3 дні у місті {message.text.title()}:\n"
         else:
-            msg = f"Погода на тиждень у місті {message.text.title()}:\n"
+            msg = f"Погода на найближчі 5 днів у місті {message.text.title()}:\n"
 
-        for day in response["daily"]:
-            c_day = datetime.fromtimestamp(day["dt"]).strftime("%d %B, %A")
-            days.append(c_day)
-            temp_day[c_day] = round(day["temp"]["day"])
-            temp_night[c_day] = round(day["temp"]["night"])
-            humidity[c_day] = round(day["humidity"])
-            wind_speed[c_day] = round(day["wind_speed"])
-            pressure[c_day] = round(day["pressure"] * 0.75006)
-            description[c_day] = day["weather"][0]["description"]
-            weather_emoji[c_day] = get_emoji.getemoji(day["weather"][0]["id"])
+        for day in response["list"]:
+            data_hour = datetime.fromtimestamp(day["dt"]).strftime("%H")
+            data_day = datetime.fromtimestamp(day["dt"]).strftime("%d")
+            if (
+                    (int(day_today) < int(data_day) <= int(day_today) + c_days + 1)
+                    and data_hour in weather_hours
+                    or (int(data_day) == int(data_day) + + c_days + 1 and data_hour == "00")
+            ):
+                c_day = datetime.fromtimestamp(day["dt"]).strftime("%d %B, %A")
+                if data_hour == "12":
+                    days.append(c_day)
+                    temp_day[c_day] = round(day["main"]["temp"])
+                    humidity[c_day] = round(day["main"]["humidity"])
+                    wind_speed[c_day] = round(day["wind"]["speed"])
+                    pressure[c_day] = round(day["main"]["pressure"] * 0.75006)
+                    description[c_day] = day["weather"][0]["description"]
+                    weather_emoji[c_day] = get_emoji.getemoji(day["weather"][0]["id"])
+                if data_hour == "00":
+                    temp_night[c_day] = round(day["main"]["temp"])
 
             if c_days == 3 and len(days) == 3:
                 break
@@ -252,7 +262,7 @@ def weather(message):
         bot.register_next_step_handler(msg, couple_days_weather, 3)
 
 
-@bot.message_handler(commands=["weekweather"])
+@bot.message_handler(commands=["5daysweather"])
 def weather(message):
     msg_logs = (
         f'{datetime.today().strftime("%Y-%m-%d %H:%M")} '
@@ -266,7 +276,7 @@ def weather(message):
 
     if user_cities is None:
         msg = bot.send_message(message.chat.id, f"Введіть назву міста:")
-        bot.register_next_step_handler(msg, couple_days_weather, 7)
+        bot.register_next_step_handler(msg, couple_days_weather, 5)
     else:
         cities = user_cities["cities"].split(", ")
         markup = telebot.types.ReplyKeyboardMarkup(
--- weatherAPI.py
+++ weatherAPI.py
@@ -9,7 +9,7 @@ API_GEO_URL = "https://api.openweathermap.org/geo/1.0"
 API_WEATHER_URL = "https://api.openweathermap.org/data/2.5"
 
 
-def get_weather(city):
+def get_weather(city: str, uri: str):
     parameters = {
         "q": city,
         "appid": WEATHER_API_KEY,
@@ -21,14 +21,14 @@ def get_weather(city):
         try:
             lat = response.json()[0]["lat"]
             lon = response.json()[0]["lon"]
-            return get_full_weather(lat, lon)
+            return get_full_weather(lat, lon, uri)
         except:
             return False
     else:
         return False
 
 
-def get_full_weather(lat, lon):
+def get_full_weather(lat, lon, uri: str):
     parameters = {
         "lat": lat,
         "lon": lon,
@@ -36,7 +36,7 @@ def get_full_weather(lat, lon):
         "units": "metric",
         "lang": "ua"
     }
-    response = requests.get(f"{API_WEATHER_URL}/weather", params=parameters)
+    response = requests.get(f"{API_WEATHER_URL}/{uri}", params=parameters)
 
     if response.status_code == 200:
         return response.json()
